fab50e4706bcff5e99d15de7355d852f0d419438
SW_Creeperking <sw.creeperking@gmail.com>
2022-12-01 15:45:18 -0800

    Initial commit

---
+++ InfectionSim/Circle.cs
@@ -0,0 +1,88 @@
+﻿using System.Numerics;
+using Raylib_CsLo;
+using RayWrapper.Base.GameBox;
+
+namespace InfectionSim;
+
+public class Circle
+{
+    public static int Healthy;
+    public static int Sick;
+    public static Random r = new();
+
+    public bool Infected
+    {
+        get => _infected;
+        set
+        {
+            if (_infected = value)
+            {
+                Sick++;
+                _infectedTimer = Program.maxInfectTimer;
+            }
+            else Healthy++;
+        }
+    }
+
+    public float Radius { get; set; }
+    public Vector2 Position { get; set; }
+    public Vector2 Velocity { get; set; }
+
+    private bool _infected;
+    private float _infectedTimer;
+    
+    public Circle(float radius, float velocityAllocation = 125)
+    {
+        var phi = 2 * Math.PI * r.NextDouble();
+        var vx = .25f * Math.Cos(phi);
+        var vy = .25f * Math.Sin(phi);
+        
+        Radius = radius;
+
+        var windowSize = GameBox.WindowSize;
+        Position = new Vector2(r.Next((int) windowSize.X), r.Next((int) windowSize.Y));
+        Velocity = new Vector2((float) vx, (float) vy) * velocityAllocation;
+    }
+
+    public void Update(float dt)
+    {
+        if (Infected)
+        {
+            _infectedTimer -= dt;
+            if (_infectedTimer <= 0) Cured();
+        }
+        
+        Position += Velocity * dt;
+
+        var windowSize = GameBox.WindowSize;
+        if (Position.X < 0 || Position.X > windowSize.X)
+        {
+            Position = Position with { X = Position.X < 0 ? windowSize.X : 0 };
+        }
+        
+        if (Position.Y < 0 || Position.Y > windowSize.Y)
+        {
+            Position = Position with { Y = Position.Y < 0 ? windowSize.Y : 0 };
+        }
+    }
+
+    public void Render()
+    {
+        Raylib.DrawCircle((int) Position.X, (int) Position.Y, Radius, Infected ? Raylib.RED : Raylib.GREEN);
+    }
+
+    public void GetInfected()
+    {
+        if (r.NextDouble() < Program.infectChance)
+        {
+            Healthy--;
+            Infected = true;
+        }
+    }
+
+    public void Cured()
+    {
+        Sick--;
+        Infected = false;
+    }
+}
---
+++ InfectionSim/Program.cs
@@ -0,0 +1,119 @@
+﻿using System.Numerics;
+using ImGuiNET;
+using InfectionSim;
+using Raylib_CsLo;
+using RayWrapper.Base.GameBox;
+using RayWrapper.Imgui.Widgets;
+
+new GameBox(new Program(), new Vector2(1280, 720), "Infection simulator");
+
+// challenge: write from 'scratch' only given render/update loop + imgui for gui
+public partial class Program : GameLoop
+{
+    public static bool paused;
+    public static bool spawnSick;
+    public static float maxInfectTimer = 40;
+    public static float simSpeed = 1;
+    public static float radius = 15;
+    public static float infectChance = .5f;
+    public static List<Circle> circles = new();
+
+    private Func<bool> _onPause;
+    private Func<bool> _onSick;
+    private List<Circle> _activeInterval = new();
+
+    public override void Init()
+    {
+        var modWindow = new CompoundWidgetBuilder()
+            .AddCheckBox("Pause Sim", out _onPause)
+            .AddSlider("Sim Speed", new SliderFloat(simSpeed, f => simSpeed = f, .5f, 2))
+            .AddSlider("Infect%", new SliderFloat(infectChance, f => infectChance = f, .01f, 1))
+            .AddNonWidget(() =>
+            {
+                if (!ImGui.InputFloat("Infect time (sec)", ref maxInfectTimer, 5, 10)) return;
+                if (maxInfectTimer <= 0) maxInfectTimer = 1;
+            })
+            .AddNonWidget(ImGui.Separator)
+            .AddCheckBox("Is Sick?", out _onSick)
+            .AddSlider("Ball Radius", new SliderFloat(radius, f => radius = f, 5, 30))
+            .AddButton("Spawn Ball", () => circles.Add(new Circle(radius) { Infected = spawnSick }));
+
+        RegisterGameObj(modWindow.ToWindow("Sim Modification Window"));
+    }
+
+    public override void UpdateLoop(float dt)
+    {
+        var modDt = dt;
+
+        if (_onPause()) paused = !paused;
+        if (_onSick()) spawnSick = !spawnSick;
+        if (paused) modDt *= 0;
+        else modDt *= simSpeed;
+
+        circles.ForEach(c => c.Update(modDt));
+
+        // meta collision (broad phase) (sweep and prune)
+        float xLeft;
+        foreach (var cirlce in circles.OrderBy(c => c.Position.X))
+        {
+            if (!_activeInterval.Any())
+            {
+                _activeInterval.Add(cirlce);
+                continue;
+            }
+
+            xLeft = cirlce.Position.X - cirlce.Radius;
+            _activeInterval.RemoveAll(c => c.Position.X + c.Radius < xLeft);
+            if (_activeInterval.Any())
+            {
+                foreach (var circleCollide in _activeInterval.Where(circleCollide =>
+                             CollisionCheck(cirlce, circleCollide)))
+                {
+                    if (cirlce.Infected ^ circleCollide.Infected)
+                    {
+                        if (cirlce.Infected) circleCollide.GetInfected();
+                        else cirlce.GetInfected();
+                    }
+
+                    VelocityUpdate(cirlce, circleCollide);
+                }
+            }
+
+            _activeInterval.Add(cirlce);
+        }
+
+        _activeInterval.Clear();
+    }
+
+    public override void RenderLoop()
+    {
+        Raylib.DrawFPS(10, 10);
+        Raylib.DrawText($"Healthy (G): {Circle.Healthy}", 10, 50, 24, Raylib.GREEN);
+        Raylib.DrawText($"Sick (K): {Circle.Sick}", 10, 80, 24, Raylib.RED);
+        circles.ForEach(c => c.Render());
+    }
+
+    public bool CollisionCheck(Circle c1, Circle c2) // narrow phase
+    {
+        var deltaX = c2.Position.X - c1.Position.X;
+        var deltaY = c2.Position.Y - c1.Position.Y;
+        var rad = c1.Radius + c2.Radius;
+        return deltaX * deltaX + deltaY * deltaY <= rad * rad;
+    }
+
+    public void VelocityUpdate(Circle c1, Circle c2)
+    {
+        var newVel1 = CalculateVelocity(c1.Velocity, c2.Velocity, c1.Position, c2.Position);
+        var newVel2 = CalculateVelocity(c2.Velocity, c1.Velocity, c2.Position, c1.Position);
+        c1.Velocity = newVel1;
+        c2.Velocity = newVel2;
+    }
+
+    public Vector2 CalculateVelocity(Vector2 vel1, Vector2 vel2, Vector2 pos1, Vector2 pos2)
+    {
+        var dot = Vector2.Dot(vel1 - vel2, pos1 - pos2);
+        var linalgNorm = (pos1 - pos2).Length();
+
+        return vel1 - dot / (linalgNorm * linalgNorm) * (pos1 - pos2);
+    }
+}
